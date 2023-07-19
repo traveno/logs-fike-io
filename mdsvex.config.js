c467f285f2c2d4b95f42382574822a403b419cff
Stephen <teamgreensf@gmail.com>
2023-07-18 17:56:52 -0700

    svelte-add mdsvex

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,13 +1,14 @@
-import { defineMDSveXConfig as defineConfig } from 'mdsvex';
-import { fileURLToPath } from 'url';
-import * as path from 'path';
-
-const dirname = path.resolve(fileURLToPath(import.meta.url), '../');
+import { defineMDSveXConfig as defineConfig } from "mdsvex";
 
 const config = defineConfig({
-	extensions: ['.md', '.svx'],
-	smartypants: { dashes: 'oldschool' },
-	layout: { blog: path.join(dirname, './src/routes/blog/+layout.svelte') }
+  extensions: [".svelte.md", ".md", ".svx"],
+
+  smartypants: {
+    dashes: "oldschool",
+  },
+
+  remarkPlugins: [],
+  rehypePlugins: [],
 });
 
 export default config;
