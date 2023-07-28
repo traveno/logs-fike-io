989190949566248049e25664f00deb6d24257aff
Stephen <teamgreensf@gmail.com>
2023-07-28 04:33:22 -0700

    The chance die is alive!

--- src/lib/components/PageCorner.svelte
+++ src/lib/components/PageCorner.svelte
@@ -1,153 +1,295 @@
 <script lang="ts">
   import { sleep } from "$lib/utils";
+  import noMouth from "$lib/assets/no-mouth.png";
+  import die from "$lib/assets/die.webp";
+  import { arrow, createFloatingActions } from "svelte-floating-ui";
+  import { offset } from "@floating-ui/core";
+  import { fade } from "svelte/transition";
   import { onMount } from "svelte";
-  import sweatSmile from '$lib/assets/sweat-smile.png';
-  import noMouth from '$lib/assets/no-mouth.png';
-  import rocket from '$lib/assets/rocket.png';
+    import { writable } from "svelte/store";
 
+  // Control vars
+  // Can the user see the corner?
   export let isVisible = false;
-
+  // Can the user interact with the corner?
+  let isReactive = false;
+  // Is the user hovering? (i.e., interacting)
+  let isUserHovering = false;
+  // Overall scale of the peel when visible
   let scale = 1.0;
-  let content = noMouth;
-  let rotate = 12;
-  let animated = false;
+  // Scale of the peel when interacted with
+  let interactScale = 3.125 * scale;
+  // Size in pixels for width and height
+  let cornerSize = 64;
 
-  $: cssSize = `width: ${isVisible ? 64 * scale + 'px' : '0px'}; height: ${isVisible ? 64 * scale + 'px' : '0px'};`;
+  // Tooltip
+  const arrowRef = writable<HTMLElement>();
+  const [floatRef, floatContent] = createFloatingActions({
+    strategy: "absolute",
+    placement: "left",
+    middleware: [
+      offset(15),
+      arrow({ element: arrowRef })
+    ],
+    onComputed({ placement, middlewareData }) {
+      const { x, y } = middlewareData.arrow!;
+      const staticSide = {
+        top: 'bottom',
+        right: 'left',
+        bottom: 'top',
+        left: 'right',
+      }[placement.split('-')[0]];
 
-  onMount(() => {
-    setTimeout(() => {
-      peak();
-    }, 4000);
+      Object.assign($arrowRef.style, {
+        left: x != null ? `${x}px` : "",
+        top: y != null ? `${y}px` : "",
+        [staticSide!]: "-6px"
+      });
+    }
   });
 
+  let tooltipVisible = false;
+  let tooltipText = 'roll the dice?';
+
+  // Regarding the content within the page peel
+  let content = noMouth;
+  let emojiRotation = 12;
+  let rocketAnimated = false;
+
+  $: currentCornerSize = isVisible
+    ? cornerSize * (isReactive && isUserHovering ? interactScale : scale)
+    : 0;
+  $: cssCorner = `width: ${currentCornerSize}px; height: ${currentCornerSize}px;`;
+  $: if (isVisible) peak();
+
   async function peak() {
-    scale = 2.0;
-    await sleep(1500);
-    scale = 1.0;
+    if ((localStorage.peeked ?? false) === false) {
+      localStorage.peeked = true;
+      console.log("I was not peeked at");
+      await sleep(2000);
+      scale = 2.0;
+      await sleep(1500);
+      scale = 1.0;
+      await sleep(600);
+    }
+
+    // emojiRotation = 0;
+    content = die;
+    rocketAnimated = true;
+    isReactive = true;
+  }
+
+  async function interact() {
+    tooltipVisible = false;
     await sleep(500);
-    rotate = 0;
-    content = rocket;
-    animated = true;
+    isReactive = false;
   }
 </script>
 
-<div class="notrans absolute top-0 right-0">
-  <div class="container relative hover:!w-[200px] hover:!h-[200px]" style="{cssSize}">
-    
+<div class="notrans floating absolute z-20" style="animation-delay: 50ms;">
+  
+  {#if tooltipVisible}
+  
+    <div
+      
+      class="absolute"
+      use:floatContent
+      in:fade={{ duration: 300, delay: 500 }}
+      out:fade={{ duration: 300 }}
+    >
+      <div class="arrow absolute z-10" bind:this={$arrowRef} />
+      <div id="tooltip" class="px-3 py-2 w-max rounded-lg typewriter flex flex-row justify-start">
+        <h1 class="!font-mono !text-neutral-100 font-bold text-xl">{tooltipText}</h1>
+      </div>
+    </div>
+  {/if}
+</div>
 
-    <div class="absolute corner top-0 right-0 left-0 bottom-0 overflow-hidden rounded-tr-xl rounded-bl-xl z-10">
-      <div class="graph absolute top-0 right-0 select-none w-72 h-72 heropattern-graphpaper-blue-400/50">
-        <div class="absolute top-0 right-0 p-5 cursor-pointer -scale-x-100 w-32 h-32" style="rotate: {rotate}deg;">
-          <img src={content} class="drop-shadow-lg {animated ? 'floating' : ''}" />
+
+<div class="notrans absolute top-0 right-0 rounded-tr-lg rounded-bl-lg ring-1 ring-black/5 dark:ring-white/5">
+  <div
+    class="container relative {isReactive ? 'hoverable' : ''}"
+    style={cssCorner}
+    on:mouseenter={() => isUserHovering = true}
+    on:mouseleave={() => isUserHovering = false}
+    role="none"
+  >
+    <div
+      class="absolute corner top-0 right-0 left-0 bottom-0 overflow-hidden  z-10"
+    >
+      <div
+        class="graph absolute top-0 right-0 select-none w-72 h-72"
+      >
+        <div
+          class="absolute top-0 right-0 m-6 w-20 h-20 transition duration-500 {isReactive
+            ? 'cursor-pointer hover:scale-110 '
+            : ''}"
+          role="none"
+          on:click={() => interact()}
+          on:mouseenter={() => tooltipVisible = isUserHovering && isReactive}
+          on:mouseleave={() => tooltipVisible = false}
+        >
+          <div id="content" class="{rocketAnimated ? "floating" : ""}">
+            <img
+              src={content}
+              class="pointer-events-none select-none"
+              style="rotate: {emojiRotation}deg;"
+              alt="Surprise!"
+              use:floatRef
+            />
+          </div>
         </div>
       </div>
     </div>
 
-    <div class="absolute curl top-0 right-0 left-0 bottom-0 rounded-bl-xl ring-1 ring-slate-900/5 dark:ring-white/5"></div>
+    <div
+      class="absolute curl top-0 right-0 left-0 bottom-0 rounded-bl-lg"
+    />
   </div>
 </div>
 
 
+
 <style lang="postcss">
-  .floating { 
+  .arrow {
+    width: 14px;
+    height: 14px;
+    rotate: 45deg;
+    background: linear-gradient(-45deg, #5c7adc, #617cd7 100%);
+    border-top: 3px solid;
+    border-right: 3px solid;
+    @apply border-neutral-100 rounded-tr;
+  }
+
+  #tooltip {
+    /* background-color: #4a6de5; */
+    border: 3px solid;
+    background: linear-gradient(to top, #4a6de5, #718adc 100%);
+    @apply !text-neutral-100 border-neutral-100;
+  }
+
+  .hoverable #content:hover, #tooltip {
+    filter: drop-shadow(0 15px 20px rgba(0, 0, 0, 0.35));
+  }
+
+  #content.floating {
+    transition: all 0.2s ease-in-out;
+  }
+
+  .typewriter h1 {
+    overflow: hidden; /* Ensures the content is not revealed until the animation */
+    border-right: .15em solid black; /* The typwriter cursor */
+    white-space: nowrap; /* Keeps the content on a single line */
+    /* margin: 0 auto; Gives that scrolling effect as the typing happens */
+    letter-spacing: .10em; /* Adjust as needed */
+    animation: 
+      typing 1.0s 0.5s steps(14, end),
+      blink-caret .9s  infinite;
+  }
+
+  /* The typing effect */
+  @keyframes typing {
+    from { width: 0 }
+    to { width: 100% }
+  }
+
+  /* The typewriter cursor effect */
+  @keyframes blink-caret {
+    from, to { border-color: transparent }
+    50% { border-color: rgb(245, 245, 245); }
+  }
+
+  .floating {
     animation-name: floating;
     animation-duration: 2s;
     animation-iteration-count: infinite;
     animation-timing-function: ease-in-out;
-  }
- 
-  @keyframes floating {
-      0% { transform: translate(0,  0px); }
-      50%  { transform: translate(0, -10px); }
-      100%   { transform: translate(0, -0px); }   
+
+    @keyframes floating {
+      0% {
+        transform: translate(0, 0px);
+      }
+      50% {
+        transform: translate(0, -5px);
+      }
+      100% {
+        transform: translate(0, 0px);
+      }
+    }
   }
 
   .graph {
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1) inset;
-    background-color: #4A6DE5;
+    box-shadow: -10px 10px 50px rgba(17, 65, 175, 0.5) inset;
+    /* background-color: #f4f4f4; */
+    @apply bg-blue-500 heropattern-graphpaper-blue-400/50;
+    /* @apply heropattern-graphpaper-blue-400/50; */
   }
 
   .container {
-    transition: all .5s ease;
+    transition: all 0.5s ease;
+
+    &.hoverable:hover {
+      width: 200px;
+      height: 200px;
+    }
   }
 
   .corner {
-    mask-image: linear-gradient(45deg, transparent, transparent 50%, white 50%, white 100%);
-  }
-
-  .corner:hover + .curl {
-    box-shadow : 0 0 10px rgba(0, 0, 0, .3);
-    &::before {
-      box-shadow: 10px 17px 35px rgba(0, 0, 0, .25);
-    }
-    
-    &::after {
-      box-shadow: 5px 15px 35px rgba(0, 0, 0, .25);
-    }
-  }
-
-  :global(.dark) {
-    .corner:hover + .curl {
-      box-shadow : 0 0 10px rgba(0, 0, 0, .6);
-      &::before {
-        box-shadow: 10px 17px 35px rgba(0, 0, 0, .8);
-      }
-      
-      &::after {
-        box-shadow: 5px 15px 35px rgba(0, 0, 0, .8);
-      }
-    }
-
-    .curl {
-      transition: all .5s ease;
-      /* edge of window */
-      background: 
-        linear-gradient(
-          225deg, 
-          #171717,
-          #171717 45%, 
-          #111111 50%, 
-          #0A0A0A 50%, 
-          #1a1a1a 56%, 
-          #202020 62%, 
-          #252525 80%,
-          #2A2A2A 100%
-        );
-      /* edge of the curled tip */
-
-      &::before {
-        box-shadow: 10px 17px 20px rgba(0, 0, 0, .5);
-      }
-      &::after {
-        box-shadow: 5px 15px 20px rgba(0, 0, 0, .5);
-      }
-    }
+    mask-image: linear-gradient(
+      45deg,
+      transparent,
+      transparent 50%,
+      white 50%,
+      white 100%
+    );
   }
 
   .curl {
     /* edge of window */
-    background: 
-      linear-gradient(
-        225deg, 
-        #ffffff,
-        #f3f3f3 45%, 
-        #000000 50%, 
-        #c1c1c1 50%, 
-        #d3d3d3 56%, 
-        #e6e6e6 62%, 
-        #f3f3f3 80%,
-        #ffffff 100%
-      );
+    background: linear-gradient(
+      225deg,
+      #ffffff,
+      #f3f3f3 45%,
+      #000000 50%,
+      #b3b3b3 50%,
+      #d3d3d3 56%,
+      #e6e6e6 62%,
+      #f3f3f3 80%,
+      #eeeeee 100%
+    );
     /* edge of the curled tip */
 
+    :global(.dark) & {
+      /* edge of window */
+      background: linear-gradient(
+        225deg,
+        #171717,
+        #171717 45%,
+        #111111 50%,
+        #0a0a0a 50%,
+        #1a1a1a 56%,
+        #202020 62%,
+        #252525 80%,
+        #212121 100%
+      );
+      /* edge of the curled tip */
+    }
 
-    transition: all .5s ease;
-    box-shadow : 0 0 5px rgba(0, 0, 0, .1);
+    transition: all 0.5s ease;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+
+    /* Hoverable effects both light and dark mode */
+    .hoverable:hover & {
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+      :global(.dark) & {
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);
+      }
+    }
 
     &::before {
-      transition: all .5s ease;
+      transition: all 0.5s ease;
       /* bottom */
-      content: '';
+      content: "";
       position: absolute;
       z-index: -1;
       right: -10%;
@@ -156,14 +298,27 @@
       max-width: 300px;
       max-height: 100px;
       height: 40%;
-      box-shadow: 10px 17px 20px rgba(0, 0, 0, .15);
+      box-shadow: 10px 17px 20px rgba(0, 0, 0, 0.15);
       transform: skew(0deg) rotate(10deg);
+
+      /* Dark mode */
+      :global(.dark) & {
+        box-shadow: 10px 17px 20px rgba(0, 0, 0, 0.5);
+      }
+
+      /* Hoverable effects both light and dark mode */
+      .hoverable:hover & {
+        box-shadow: 10px 17px 35px rgba(0, 0, 0, 0.25);
+        :global(.dark) & {
+          box-shadow: 10px 17px 35px rgba(0, 0, 0, 0.8);
+        }
+      }
     }
-    
+
     &::after {
-      transition: all .5s ease;
+      transition: all 0.5s ease;
       /* left */
-      content: '';
+      content: "";
       position: absolute;
       z-index: -1;
       right: auto;
@@ -174,8 +329,21 @@
       max-width: 300px;
       max-height: 100px;
       height: 40%;
-      box-shadow: 5px 15px 20px rgba(0, 0, 0, .2);
+      box-shadow: 5px 15px 20px rgba(0, 0, 0, 0.2);
       transform: skew(0deg) rotate(80deg);
+
+      /* Dark mode */
+      :global(.dark) & {
+        box-shadow: 5px 15px 20px rgba(0, 0, 0, 0.5);
+      }
+
+      /* Hoverable effects both light and dark mode */
+      .hoverable:hover & {
+        box-shadow: 5px 15px 35px rgba(0, 0, 0, 0.25);
+        :global(.dark) & {
+          box-shadow: 5px 15px 35px rgba(0, 0, 0, 0.8);
+        }
+      }
     }
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,60 +1,33 @@
 <script lang="ts">
-  import { convertDate } from '$lib/utils.js';
-  import Tag from '$lib/components/Tag.svelte';
-  
+  import { convertDate } from "$lib/utils.js";
+  import Tag from "$lib/components/Tag.svelte";
+  import RecentPosts from "./RecentPosts.svelte";
+
   export let data;
   const { posts } = data;
 
-  $: tags = posts.flatMap(p => p.tags);
+  $: tags = posts.flatMap((p) => p.tags);
 </script>
 
 <svelte:head>
-<title>Index | logs.fike.io</title>
+  <title>Index | logs.fike.io</title>
 </svelte:head>
 
 <div class=" px-4 md:px-32">
   <div class="w-full">
     <div class="pb-8">
-      <h1 class="text-3xl font-mono font-thin border-b border-neutral-300 pb-2">tags</h1>
+      <h1 class="text-3xl font-mono font-thin border-b border-neutral-300 pb-2">
+        tags
+      </h1>
       <div class="py-4 flex flex-row gap-2 flex-wrap justify-center">
-       {#each tags as tag}
-         <span class="inline-block w-fit">
-           <Tag name={tag} />
-         </span>
-       {/each}
+        {#each tags as tag}
+          <span class="inline-block w-fit">
+            <Tag name={tag} />
+          </span>
+        {/each}
       </div>
     </div>
 
-
-    <div class="border-b border-b-neutral-300 pb-4">
-      <h1 class="text-3xl font-mono font-thin">recent posts</h1>
-    </div>
-    <div class="mt-8 grid gap-4 lg:grid-cols-2">
-      {#each posts || [] as post}
-      <a href="/{post.slug}">
-        <div class="border border-neutral-300 p-4 rounded-lg flex flex-col justify-between transition text-neutral-700 hover:shadow-lg hover:scale-[1.01]">
-          <p class="text-xl">{post.title}</p>
-          <p class="mt-3 text-neutral-500">{post.blurb}</p>
-          <div class="mt-3">
-            <div class="flex flex-row justify-between items-center">
-              <div class="flex flex-row justify-start items-center gap-2">
-                <!-- {#each post.tags as tag}
-                <div class="font-mono text-sm rounded outline outline-1 outline-neutral-400 px-1 bg-neutral-200">
-                  <div class="flex flex-row items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 stroke-neutral-700">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5" />
-                    </svg>
-                    <div class="text-neutral-700">{tag}</div>
-                  </div>
-                </div>
-                {/each} -->
-              </div>
-              <div class="text-sm text-neutral-500">{convertDate(post.date)}</div>
-            </div>
-          </div>
-        </div>
-      </a>
-      {/each}
-    </div>
+    <RecentPosts {posts} />
   </div>
 </div>
